f4677099c4709202825ed8c06c33e8738b868e06
Shubhankar Kumar <shubhankur99@gmail.com>
2022-11-14 21:18:40 -0500

    Revert "saving transactions"
    This reverts commit 9a8c8070f0ecf17a1308b427c94fb0bb8e518545.

--- contract/contracts/Trade.sol
+++ contract/contracts/Trade.sol
@@ -1,18 +1,7 @@
 pragma solidity >=0.4.21 <0.7.0;
 contract Trade{
-    struct trans{
-        address buyer;
-        address seller;
-        uint value;
-    }
-    mapping (string=>trans[]) propertytransactions;
-
-    function sendMoney(address seller, string memory propertyId) public payable{
-        address payable to  = address(uint160(address(seller)));
+    function sendMoney(address buyer) public payable{
+        address payable to  = address(uint160(address(buyer)));
         address(to).transfer(msg.value);
-        trans[] storage transactions = propertytransactions[propertyId];
-        trans memory newTrans = trans(msg.sender,seller, msg.value);
-        transactions.push(newTrans);
-        propertytransactions[propertyId] = transactions;
     }
 }
